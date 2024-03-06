2d89fab7196172388bd4ca8d6c343fd329b88989
+ <+>
2024-03-06 13:35:15 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = ("victory");
+newHeader.textContent = ("GIL is the champion");
+const main = document.getElementById("main");
+main.remove();
